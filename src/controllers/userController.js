424a9be04aa50d7eb00ac8e7e3f8698275b0c457
prachikshir <pnkshir@gmail.com>
2025-01-31 14:12:15 +0530

    code

---
+++ src/controllers/userController.js
@@ -0,0 +1,18 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Register user
+exports.registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error registering user', error });
+  }
+};
+
+// Add more controller functions for login, etc.
